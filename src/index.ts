8cd5c74dc34c2d2f6074f2e5c96bcb9f61448602
asminkarki012 <asminkarki09@gmail.com>
2025-01-04 15:04:57 +0545

    cleaned

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,5 @@
-// import { parse } from "url";
-// import { getPeers } from "./tracker";
 import download from "./download";
 import * as torrentParser from "./torrent-parser";
-//torrent name should be -NT0000-
-// to run terminal for this nodex index.ts /filepath to torrent file
-const filePath: string = process.argv[2] || "torrents/wheeloftime.torrent";
-// const filePath:string = "torrents/All_Dune_books__short_stories__extras_ePUB.torrent" ;
-//
+const filePath: string = process.argv[2] || "torrents/electronics.torrent";
 const torrent = torrentParser.open(filePath);
-// text decoder can be used for browser or nodejs
-// const decoder = new TextDecoder('utf-8');
 download(torrent, torrent.info.name);
-// getPeers(torrent, (peers: any) => {
-//   console.log("list of peers:", peers);
-// })
